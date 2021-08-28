2f68d3c42f3eabbfab7e0f8601e7fe1ffb682bb2
Egor <itpd2010@yandex.ru>
2021-08-28 12:46:26 +0300

    feat: tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str) {
+  const OPEN_BRACKETS = ['(', '[', '{', '|'];
+  const bracketsConfig = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['|']: '|',
+  };
+
+  
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let strSymbol = str[i];
+
+      if (OPEN_BRACKETS.includes(strSymbol)) {
+        stack.push(strSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+
+        if (bracketsConfig[strSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+        if (topElement === '|') {
+          stack.pop();
+        } 
+        
+      }
+    }
+
+    return stack.length === 0;
+  
 }
+
